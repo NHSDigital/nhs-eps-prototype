197fe69117eb305c0a907db9cbc7b2df92eb79ae
Jess <jessica.mancoo@hippodigital.co.uk>
2024-09-04 15:51:28 +0100

    routes update

--- app/views/epsmvp/_routes.js
+++ app/views/epsmvp/_routes.js
@@ -1,16 +1,16 @@
 module.exports = (router) => {
 
-  router.get('/eps_mvp/search', function (req, res) {
+  router.get('/epsmvp/search', function (req, res) {
     // check for clear
     let refine = req.query.refine || 'false';
     let lastSearch = req.session.data["last-search"];
-    let target = 'eps_mvp/search';
+    let target = 'epsmvp/search';
     if (lastSearch === 'basic') {
-      target = 'eps_mvp/search-basic';
+      target = 'epsmvp/search-basic';
     } else if (lastSearch === 'advanced') {
-      target = 'eps_mvp/search-advanced';
+      target = 'epsmvp/search-advanced';
     } else {
-      target = 'eps_mvp/search';
+      target = 'epsmvp/search';
     }
 
     return res.render(target, {
@@ -20,7 +20,7 @@ module.exports = (router) => {
 
   //search via presc number
 // search via presc number form
-router.post("/eps_mvp/search-presc-post", function (req, res) {
+router.post("/epsmvp/search-presc-post", function (req, res) {
   const prescNumber = req.body["prescNumber"];
   req.session.data.errors = {};
 
@@ -61,7 +61,7 @@ router.post("/eps_mvp/search-presc-post", function (req, res) {
 });
 
   // search via nhs number form
-  router.post("/eps_mvp/search-nhs-post", function (req, res) {
+  router.post("/epsmvp/search-nhs-post", function (req, res) {
     let nhsNumber = req.body["nhsNumber"].replace(/\s/g, ''); // Remove spaces on server side
     req.session.data.errors = {};
 
@@ -97,7 +97,7 @@ router.post("/eps_mvp/search-presc-post", function (req, res) {
 
 
   // search via nhs basic search
-  router.post("/eps_mvp/search-basic-post", function (req, res) {
+  router.post("/epsmvp/search-basic-post", function (req, res) {
     let patients = req.session.data['patients']; // Assumes patients data is stored in session
 
     let searchPostcode = req.body["postcode-only"];
@@ -160,7 +160,7 @@ router.post("/eps_mvp/search-presc-post", function (req, res) {
 
 
 // change record via a NHS number on NoK section
-  router.post("/eps_mvp/change-record", function (req, res) {
+  router.post("/epsmvp/change-record", function (req, res) {
     const nhsNumber = req.body["change-by-nhs"];
     req.session.data.errors = {};
 
@@ -191,7 +191,7 @@ router.post("/eps_mvp/search-presc-post", function (req, res) {
   });
 
 
-  router.post("/eps_mvp/switch-layout", function (req, res) {
+  router.post("/epsmvp/switch-layout", function (req, res) {
     let columns = req.session.data["layout"];
 
     if (columns === 'oneCol') {
@@ -207,27 +207,27 @@ router.post("/eps_mvp/search-presc-post", function (req, res) {
     res.redirect(`patient-details`);
   });
 
-  router.get('/eps_mvp/patient-details', function (req, res) {
+  router.get('/epsmvp/patient-details', function (req, res) {
     const target = req.query.tab;
     if (target) {
 
     } else {
 
     }
-    return res.render('eps_mvp/patient-details', {'target': target})
+    return res.render('epsmvp/patient-details', {'target': target})
   })
 
   // switcher routing
 
-  router.post('/eps_mvp/switch', function (req, res) {
+  router.post('/epsmvp/switch', function (req, res) {
     // grab the bit of the url we need to se the redirect to the same page
     var url = req.get('Referrer') .split( '/' );
-    res.redirect('/eps_mvp/' + url[4] )
+    res.redirect('/epsmvp/' + url[4] )
   })
  
   
  //search results page
-  router.get("/eps_mvp/search-results", function (req, res, next) {
+  router.get("/epsmvp/search-results", function (req, res, next) {
     // Get the NHS number from the query parameters
     const nhsNumber = req.query["nhsNumber"] ? req.query["nhsNumber"].trim() : '';
     console.log('Received NHS Number:', nhsNumber);
@@ -259,7 +259,7 @@ router.post("/eps_mvp/search-presc-post", function (req, res) {
     console.log('Returned Patients List:', returnedPatientsList);
 
     // Render the search results page with the returned patient list
-    res.render('./eps_mvp/search-results', {
+    res.render('./epsmvp/search-results', {
         returnedPatientsList: returnedPatientsList
     });
 });
