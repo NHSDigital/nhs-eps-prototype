250c9659445ad587ef0035242fdd3b6a574e9a8e
Jess <jessica.mancoo@hippodigital.co.uk>
2025-02-05 13:31:50 +0000

    new erd in

--- app/views/epsv12/_routes.js
+++ app/views/epsv12/_routes.js
@@ -270,7 +270,7 @@ router.get("/epsv12/search-results", function (req, res, next) {
       const prescription = item[key];
       
       // how we filter the list down
-      return prescription.prescriptionStatus !== 'Future issue date' && prescription.prescriptionStatus !== 'Future repeat dispense' && prescription.prescriptionStatus !== 'Expired' && prescription.prescriptionStatus !== 'Claimed';;
+      return prescription.prescriptionStatus !== 'Future issue date' && prescription.prescriptionStatus !== 'Future repeat dispense'&& prescription.prescriptionStatus !== 'Future eRD issue' && prescription.prescriptionStatus !== 'Expired' && prescription.prescriptionStatus !== 'Claimed';;
     }).map(item => {
       const key = Object.keys(item)[0];
       const prescription = item[key];
@@ -293,7 +293,7 @@ router.get("/epsv12/search-results", function (req, res, next) {
       const key = Object.keys(item)[0];
       const prescription = item[key];
       // how we filter the list down
-      return prescription.prescriptionStatus === 'Future issue date' || prescription.prescriptionStatus === 'Future repeat dispense';
+      return prescription.prescriptionStatus === 'Future issue date' || prescription.prescriptionStatus === 'Future repeat dispense'|| prescription.prescriptionStatus === 'Future eRD issue';
     }).map(item => {
       const key = Object.keys(item)[0];
       const prescription = item[key];
